c7c07fc86da827bd83ea5e7e0a91bea1db7986e2
joelyustiz <yustizjoel17@gmail.com>
2019-11-25 22:38:50 -0600

    agregando dependencias

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+
+const url = "https://ws-api.iextrading.com/1.0/tops";
+const socket = io(url);
 
 function App() {
+
+  const getTable = async () => {
+    try {
+      socket.emit("subscribe", "goog,fb,appl,snap");
+      socket.on('connect', () => {
+        console.log("connectte");
+        
+      });
+
+      socket.on("subscribe", () => {
+        console.log("cssssssssss")
+      })
+    } catch (error) {
+     
+    }
+
+  }
+
+  useEffect(()=>{
+    getTable()
+   
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
